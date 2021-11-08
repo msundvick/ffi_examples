1107c1ae3cb954ca901e39b226d0a53bab3803a0
Michael Sundvick <msundvick@sailgp.com>
2021-11-08 18:01:24 +1300

    Get build working

--- build.py
+++ build.py
@@ -18,59 +18,9 @@ logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO"))
 
 def build(setup_kwargs):
 
-    logging.info("Deleting nim files")
-    Path("./MANIFEST.in").unlink(missing_ok=True)
-    shutil.rmtree("./nim-extensions")
-
-    logging.info("Before: %s", setup_kwargs)
-    name = setup_kwargs["packages"][0]
-
-    extensions: List[Union[Extension, Pybind11Extension]] = [
-        Extension(
-            f"{name}.c_example",
-            [f"{name}/c/example.c"],
-            language="c",
-            py_limited_api=True,
-            define_macros=[("Py_LIMITED_API", None)],
-        ),
-        Pybind11Extension(
-            f"{name}.pybind_example",
-            [f"{name}/cpp/cpp_example.cpp"],
-        ),
-    ]
-
-    # Build
-    setup_kwargs.update(
-        {
-            "ext_modules": cythonize(
-                f"{name}/**/*_cy.py",
-                language_level=3,
-                annotate=True,
-                compiler_directives={"linetrace": True},
-            )
-            + extensions
-            + nimporter.build_nim_extensions(),
-            "rust_extensions": [
-                RustExtension(
-                    f"{name}/rust",
-                    path="Cargo.toml",
-                    binding=Binding.PyO3,
-                    debug=False,
-                )
-            ],
-            "install_requires": ["numpy", "setuptools_rust"],
-            "include_dirs": [numpy.get_include()],
-            "zip_safe": False,
-        }
-    )
-
-    logging.info("After: %s", setup_kwargs)
-
-    with open("juliacalldeps.json", "r") as f:
+    with open("ffi_examples/juliacalldeps.json", "r") as f:
         jl = json.load(f)
 
     jl["packages"]["FfiExamples"]["path"] = os.getcwd() + "/ffi_examples/FfiExamples"
-    with open("juliacalldeps.json", "w") as f:
+    with open("ffi_examples/juliacalldeps.json", "w") as f:
         json.dump(jl, f)
-
-    import juliacall
